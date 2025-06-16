85b9770defe3b1e8b34a3178f1c35ee7b8c475d8
Maura Fortino <maurafortino@Mauras-MacBook-Pro-2.local>
2025-06-15 23:05:25 -0400

    updated styling

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import {Image, Text, View} from "react-native";
+import { Link } from 'expo-router';
+import {ScrollView} from "react-native";
+import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
+export default function Index() {
+  return (
+    <View className="flex-1 bg-primary">
+        <Image source={images.bg} className="absolute w-full z-0"/>
+        <ScrollView
+            className="flex-1 px-5"
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+                minHeigh:"100%",
+                paddingBottom:10
+            }}
+        >
+            <Image source={icons.logo} className="w-12 h-10 my-20 mb-5 mx-auto" />
+        </ScrollView>
+    </View>
+  );
+}
