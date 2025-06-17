2fa9f10dc4444e314c447b8d438b0ec2647c2d98
Maura Fortino <maurafortino@gmail.com>
2025-06-16 21:20:11 -0400

    added search bar component and minor functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,10 @@ import {ScrollView} from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import {useRouter} from "expo-router";
 
 export default function Index() {
+    const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
         <Image source={images.bg} className="absolute w-full z-0"/>
@@ -19,7 +21,10 @@ export default function Index() {
         >
             <Image source={icons.logo} className="w-12 h-10 my-20 mb-5 mx-auto" />
             <View className="flex-1 mt-5">
-                <SearchBar/>
+                <SearchBar
+                    onPress={()=> router.push("/search")}
+                    placeholder = "Search for a movie"
+                />
             </View>
         </ScrollView>
     </View>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,29 @@
-import {Text, View} from "react-native";
+import {Image, TextInput, View} from "react-native";
 import React from 'react';
+import {icons} from "@/constants/icons";
 
-const SearchBar = () => {
+interface Props {
+    placeholder: string;
+    onPress?: () => void;
+}
+
+const SearchBar = ({placeholder, onPress}: Props) => {
     return(
         <View className="flex-row items-center bg-dark-200 rounded0full px-5 py-4">
-            <Text>SearchBar</Text>
+            <Image
+                source={icons.search}
+                className="size-5"
+                resizeMode="contain"
+                tintColor="#ab8bff"
+            />
+            <TextInput
+                onPress={onPress}
+                placeholder={placeholder}
+                value=""
+                onChangeText={()=>{}}
+                placeholderTextColor="#a8b5db"
+                className="flex-1 ml-2 text-white"
+            />
         </View>
     )
 }
